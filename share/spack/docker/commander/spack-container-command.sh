1a1dcae29c56aed66b7d6f179c067f7cc3439467
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-12-17 17:33:46 -0700

    docker: Add Dockerfiles for images needed by AWS gitlab-runners
    The built images also contain a convenience bash script for running
    arbitrary spack commands using the current state of spack you have
    checked out on disk.

---
+++ share/spack/docker/commander/spack-container-command.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PATH_PREFIX="${1}"
+shift
+export PATH="${PATH_PREFIX}:${PATH}"
+
+SPACK_COMMAND=""
+
+for i in "${@}"
+do
+    SPACK_COMMAND="${SPACK_COMMAND} ${i}"
+done
+
+echo "Spack command: ${SPACK_COMMAND}"
+
+SPACK_COMMAND_OUTPUT=$(${SPACK_COMMAND})
+
+echo -e "<BEGIN_SPACK_COMMAND_OUTPUT>\n${SPACK_COMMAND_OUTPUT}\n<END_SPACK_COMMAND_OUTPUT>"
---
+++ share/spack/docker/commander/update_rpaths.py
@@ -0,0 +1,34 @@
+
+import argparse
+import spack.util.spack_yaml as syaml
+
+
+def update_compiler(compilers_path, prefix, rpaths):
+    with open(compilers_path, 'r') as fd:
+        compilers_text = fd.read()
+
+    compilers = syaml.load(compilers_text)
+
+    for c in compilers['compilers']:
+        if c['compiler']['paths']['cc'].startswith(prefix):
+            print('found target compiler: {0}'.format(c['compiler']['spec']))
+            c['compiler']['extra_rpaths'].append(rpaths)
+
+    with open(compilers_path, 'w') as fd:
+        fd.write(syaml.dump(compilers))
+
+
+if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Add extra_rpaths to compiler")
+
+    parser.add_argument('-c', '--compilers-yaml-path', default='/root/.spack/linux/compilers.yaml',
+        help="Absolute path to compilers.yaml to update")
+    parser.add_argument('-p', '--prefix', default=None,
+        help="Install prefix of compiler to update")
+    parser.add_argument('-r', '--rpaths', default=None,
+        help="Extra rpaths to add to target compiler")
+
+    args = parser.parse_args()
+
+    update_compiler(args.compilers_yaml_path, args.prefix, args.rpaths)
