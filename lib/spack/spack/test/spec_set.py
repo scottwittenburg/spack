fea54ffa6f951ac9b8ad776ed23e0d161a24a022
Scott Wittenburg <scott.wittenburg@kitware.com>
2019-02-11 12:40:06 -0700

    Another round of review changes

--- lib/spack/spack/test/spec_set.py
+++ lib/spack/spack/test/spec_set.py
@@ -15,6 +15,8 @@ pytestmark = pytest.mark.usefixtures('config')
 
 basic_yaml_file = {
     'spec-set': {
+        'cdash': 'http://example.com/cdash',
+        'project': 'testproj',
         'include': ['gmake'],
         'matrix': [
             {'packages': {
@@ -35,12 +37,20 @@ basic_yaml_file = {
 
 
 def test_spec_set_basic():
+    """The "include" isn't required, but if it is present, we should only
+    see specs mentioned there.  Also, if we include cdash and project
+    properties, those should be captured and stored on the resulting
+    CombinatorialSpecSet as attributes."""
     spec_set = CombinatorialSpecSet(basic_yaml_file, False)
     specs = list(spec for spec in spec_set)
     assert len(specs) == 4
+    assert spec_set.cdash == ['http://example.com/cdash']
+    assert spec_set.project == 'testproj'
 
 
 def test_spec_set_no_include():
+    """Make sure that without any exclude or include, we get the full cross-
+    product of specs/versions."""
     yaml_file = {
         'spec-set': {
             'matrix': [
@@ -64,7 +74,8 @@ def test_spec_set_no_include():
     assert len(specs) == 4
 
 
-def test_spec_set_exclude():
+def test_spec_set_include_exclude_conflict():
+    """Exclude should override include"""
     yaml_file = {
         'spec-set': {
             'include': ['gmake'],
@@ -90,7 +101,42 @@ def test_spec_set_exclude():
     assert len(specs) == 0
 
 
+def test_spec_set_exclude():
+    """The exclude property isn't required, but if it appears, any specs
+    mentioned there should not appear in the output specs"""
+    yaml_file = {
+        'spec-set': {
+            'exclude': ['gmake'],
+            'matrix': [
+                {'packages': {
+                    'gmake': {
+                        'versions': ['4.0']
+                    },
+                    'appres': {
+                        'versions': ['1.0.4']
+                    },
+                    'allinea-reports': {
+                        'versions': ['6.0.4']
+                    }
+                }},
+                {'compilers': {
+                    'gcc': {
+                        'versions': ['4.2.1', '6.3.0']
+                    }, 'clang': {
+                        'versions': ['8.0', '3.8']
+                    }
+                }},
+            ]
+        }
+    }
+    spec_set = CombinatorialSpecSet(yaml_file, False)
+    specs = list(spec for spec in spec_set)
+    assert len(specs) == 8
+
+
 def test_spec_set_include_limited_packages():
+    """If we see the include key, it is a filter and only the specs mentioned
+    there should actually be included."""
     yaml_file = {
         'spec-set': {
             'include': ['gmake'],
@@ -122,6 +168,9 @@ def test_spec_set_include_limited_packages():
 
 
 def test_spec_set_simple_spec_list():
+    """Make sure we can handle the slightly more concise syntax where we
+    include the package name/version together and skip the extra keys in
+    the dictionary."""
     yaml_file = {
         'spec-set': {
             'matrix': [
@@ -139,6 +188,7 @@ def test_spec_set_simple_spec_list():
 
 
 def test_spec_set_with_specs():
+    """Make sure we only see the specs mentioned in the include"""
     yaml_file = {
         'spec-set': {
             'include': ['gmake', 'appres'],
@@ -163,26 +213,9 @@ def test_spec_set_with_specs():
     assert len(specs) == 8
 
 
-def test_spec_set_compilers_bad_property():
-    yaml_file = {
-        'spec-set': {
-            'foobar': ['gmake'],
-            'matrix': [
-                {'packages': {
-                    'gmake': {'versions': ['4.0']},
-                }},
-                {'compilers': {
-                    'gcc': {'versions': ['4.2.1', '6.3.0']},
-                    'clang': {'versions': ['8.0', '3.8']},
-                }},
-            ]
-        }
-    }
-    with pytest.raises(ValidationError):
-        CombinatorialSpecSet(yaml_file)
-
-
 def test_spec_set_packages_no_matrix():
+    """The matrix property is required, make sure we error out if it is
+    missing"""
     yaml_file = {
         'spec-set': {
             'include': ['gmake'],
@@ -203,29 +236,8 @@ def test_spec_set_packages_no_matrix():
         CombinatorialSpecSet(yaml_file)
 
 
-def test_spec_set_get_cdash_string():
-    yaml_file = {
-        'spec-set': {
-            'cdash': 'http://example.com/cdash',
-            'project': 'testproj',
-            'matrix': [
-                {'packages': {
-                    'gmake': {'versions': ['4.0']},
-                }},
-                {'compilers': {
-                    'gcc': {'versions': ['4.2.1', '6.3.0']},
-                    'clang': {'versions': ['8.0', '3.8']},
-                }},
-            ]
-        }
-    }
-
-    spec_set = CombinatorialSpecSet(yaml_file)
-    assert spec_set.cdash == ['http://example.com/cdash']
-    assert spec_set.project == 'testproj'
-
-
 def test_spec_set_get_cdash_array():
+    """Make sure we can handle multiple cdash sites in a list"""
     yaml_file = {
         'spec-set': {
             'cdash': ['http://example.com/cdash', 'http://example.com/cdash2'],
