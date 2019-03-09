c57572955b88be8055110d4d711afeaac2abb438
Scott Wittenburg <scott.wittenburg@kitware.com>
2019-03-08 17:23:37 -0700

    Try generating the real jobs set now

--- bin/generate-ci-yml.sh
+++ bin/generate-ci-yml.sh
@@ -11,30 +11,15 @@ apt-get -qyy update && apt-get -qyy install git
 git config --global user.email "robot@spack.io"
 git config --global user.name "Mr. Roboto"
 
+micro_service_url="https://internal.spack.io/glciy/${CI_COMMIT_SHA}.yaml"
+
 # TODO: do more interesting dynamic st00f here
 echo "Generating .gitlab-ci.yml"
 echo
 tee .gitlab-ci.yml << EOF
-hello:
-  script:
-    - "./run-this-script.sh"
-  tags:
-    - "spack-k8s"
-  image: "ubuntu"
-  variables:
-    SHA: "$CI_COMMIT_SHA"
+include:
+  - remote: $micro_service_url
 EOF
-
-echo "Generating ./run-this-script.sh"
-echo
-tee ./run-this-script.sh << EOF
-#! /usr/bin/env sh
-
-echo "Upstream SHA: \$SHA"
-echo "Hello, from second CI level! ^___^"
-EOF
-
-chmod +x ./run-this-script.sh
 
 git status
 
@@ -43,7 +28,7 @@ downstream_branch="auto-ci-$current_branch"
 
 git branch -D ___multi_ci___ 2> /dev/null || true
 git checkout -b ___multi_ci___
-git add .gitlab-ci.yml ./run-this-script.sh
+git add .gitlab-ci.yml
 git commit -m 'x'
 git reset "$( git commit-tree HEAD^{tree} -m x )"
 git status
