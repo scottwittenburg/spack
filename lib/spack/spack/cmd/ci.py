89535da9e83d426c7e89a0b83037cba6005497e4
Ryan Krattiger <ryan.krattiger@kitware.com>
2023-04-21 15:24:27 -0500

    CI: Add interactive and gpg options to reproduce-build
    * Interactive will determine if the docker container persists
      after running reproduction.
    * GPG path/url allow downloading GPG keys needed for binary
      cache download validation. This is important for running
      reproducer for protected CI jobs.

--- lib/spack/spack/cmd/ci.py
+++ lib/spack/spack/cmd/ci.py
@@ -172,6 +172,19 @@ through the SPACK_CONCRETE_ENVIRONMENT_PATH variable.""",
         help="Where to unpack artifacts",
         default=os.path.join(os.getcwd(), "ci_reproduction"),
     )
+    reproduce.add_argument(
+        "-s",
+        "--autostart",
+        help="Run docker reproducer automatically",
+        action="store_true",
+    )
+    gpg_group = reproduce.add_mutually_exclusive_group(required=False)
+    gpg_group.add_argument(
+        "--gpg-file", help="Path to public GPG key for validating binary cache installs"
+    )
+    gpg_group.add_argument(
+        "--gpg-url", help="URL to public GPG key for validating binary cache installs"
+    )
 
     reproduce.set_defaults(func=ci_reproduce)
 
@@ -709,7 +722,7 @@ def ci_rebuild(args):
 
 \033[34mTo reproduce this build locally, run:
 
-    spack ci reproduce-build {0} [--working-dir <dir>]
+    spack ci reproduce-build {0} [--working-dir <dir>] [--autostart]
 
 If this project does not have public pipelines, you will need to first:
 
@@ -734,8 +747,17 @@ def ci_reproduce(args):
     used to derive instructions for reproducing the build locally."""
     job_url = args.job_url
     work_dir = args.working_dir
+    autostart = args.autostart
 
-    return spack_ci.reproduce_ci_job(job_url, work_dir)
+    # Allow passing GPG key for reprocuding protected CI jobs
+    if args.gpg_file:
+        gpg_key_url = url_util.path_to_file_url(args.gpg_file)
+    elif args.gpg_url:
+        gpg_key_url = args.gpg_url
+    else:
+        gpg_key_url = None
+
+    return spack_ci.reproduce_ci_job(job_url, work_dir, autostart, gpg_key_url)
 
 
 def ci(parser, args):
